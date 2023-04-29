8b75aadcdf14356323b976aa515d256908003e7d
yinxiaolog <15121849303@163.com>
2023-04-29 11:36:52 +0800

    add log modual

--- log/logger.py
+++ log/logger.py
@@ -1,33 +1,38 @@
 import logging.handlers
 import yaml
+import os
 
-config_path = './config.yml'
+config_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+config_path = os.path.join(config_dir, 'config.yml')
 
 
 class Logger:
-    logger = None
-    file = None
-    desc = None
+    __logger = None
+    __file = None
+    __desc = None
 
     @staticmethod
-    def get_log_config():
+    def __get_log_config():
         with open(config_path, 'r', encoding='utf-8') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-            Logger.file = config['log']['file']
-            Logger.desc = config['log']['desc']
+            Logger.__file = config['log']['file']
+            Logger.__desc = config['log']['desc']
 
     @staticmethod
     def get_logger():
-        if Logger.logger is not None:
-            return Logger.logger
+        if Logger.__logger is not None:
+            return Logger.__logger
 
-        Logger.get_log_config()
-        Logger.logger = logging.Logger("Logger")
-        if 'file' in Logger.desc:
-            file_handler = logging.handlers.RotatingFileHandler(filename=Logger.file)
-            Logger.logger.addHandler(file_handler)
-        if 'console' in Logger.desc:
+        Logger.__get_log_config()
+        Logger.__logger = logging.Logger("Logger")
+        log_fmt = logging.Formatter("[%(asctime)s][%(levelname)s][func:%(funcName)s]: %(message)s")
+        if 'file' in Logger.__desc:
+            file_handler = logging.handlers.RotatingFileHandler(filename=Logger.__file)
+            file_handler.setFormatter(log_fmt)
+            Logger.__logger.addHandler(file_handler)
+        if 'console' in Logger.__desc:
             console_handler = logging.StreamHandler()
-            Logger.logger.addHandler(console_handler)
+            console_handler.setFormatter(log_fmt)
+            Logger.__logger.addHandler(console_handler)
 
-        return Logger.logger
+        return Logger.__logger
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from log.logger import Logger
 logger = Logger.get_logger()
 
 if __name__ == '__main__':
-    logger.debug("test")
+    a = 1
+    logger.info(f'{a} test')
