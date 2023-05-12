af0e60abdb6a867f9db765f8262f8db1579e453b
yinxiaolog <15121849303@163.com>
2023-05-12 14:34:45 +0800

    learn ch3 linear regression

---
+++ d2l/ch3_3.1_linear_regression.py
@@ -0,0 +1,32 @@
+import math
+
+import numpy as np
+import torch
+from d2l import torch as d2l
+
+n = 1000000
+a = torch.ones([n])
+b = torch.ones([n])
+c = torch.zeros(n)
+timer = d2l.Timer()
+for i in range(n):
+    c[i] = a[i] + b[i]
+
+print(f'{timer.stop():.5f} sec')
+timer.start()
+d = a + b
+print(f'{timer.stop():.5f} sec')
+
+
+# 3.1.3 正态分布与均方损失
+def normal(x, mu, sigma):
+    p = 1 / math.sqrt(2 * math.pi * sigma ** 2)
+    return p * np.exp(-0.5 / sigma ** 2 * (x - mu) ** 2)
+
+
+x = np.arange(-7, 7, 0.01)
+params = [(0, 1), (0, 2), (3, 1)]
+d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params],
+         xlabel='x', ylabel='p(x)', figsize=(4.5, 2.5),
+         legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
+d2l.plt.show()
