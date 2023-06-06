965f8f0769f0158489a13632c357fa084cf8e262
yinxiaolog <15121849303@163.com>
2023-06-06 12:18:19 +0800

    add framework

--- log/logger.py
+++ log/logger.py
@@ -1,38 +1,56 @@
 import logging.handlers
-import yaml
 import os
 
-config_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-config_path = os.path.join(config_dir, 'config.yml')
+import yaml
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Logger:
     __logger = None
     __file = None
     __desc = None
+    __log_base_dir = None
+    model_name = None
+    time = None
+    log_fmt = logging.Formatter("[%(asctime)s][%(levelname)s][func:%(funcName)s]: %(message)s")
 
     @staticmethod
-    def __get_log_config():
+    def set(model_name, time):
+        Logger.model_name = model_name
+        Logger.time = time
+        Logger.__get_log_config(model_name)
+        if 'file' in Logger.__desc and Logger.model_name is not None and Logger.time is not None:
+            log_path = os.path.join(Logger.__log_base_dir,
+                                    model_name + '_' + time)
+            if not os.path.exists(log_path):
+                os.makedirs(log_path)
+
+            log_file = os.path.join(log_path, 'train.log')
+            if not os.path.exists(log_file):
+                os.mknod(log_file)
+
+            file_handler = logging.handlers.RotatingFileHandler(
+                filename=log_file)
+            file_handler.setFormatter(Logger.log_fmt)
+            Logger.__logger.addHandler(file_handler)
+        if 'console' in Logger.__desc:
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(Logger.log_fmt)
+            Logger.__logger.addHandler(console_handler)
+
+    @staticmethod
+    def __get_log_config(model_name):
+        config_path = os.path.join(base_dir, 'modelsConfig', model_name + '.yml')
         with open(config_path, 'r', encoding='utf-8') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
             Logger.__file = config['log']['file']
             Logger.__desc = config['log']['desc']
+            Logger.__log_base_dir = config['log']['log_base_dir']
 
     @staticmethod
     def get_logger():
         if Logger.__logger is not None:
             return Logger.__logger
-
-        Logger.__get_log_config()
         Logger.__logger = logging.Logger("Logger")
-        log_fmt = logging.Formatter("[%(asctime)s][%(levelname)s][func:%(funcName)s]: %(message)s")
-        if 'file' in Logger.__desc:
-            file_handler = logging.handlers.RotatingFileHandler(filename=Logger.__file)
-            file_handler.setFormatter(log_fmt)
-            Logger.__logger.addHandler(file_handler)
-        if 'console' in Logger.__desc:
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(log_fmt)
-            Logger.__logger.addHandler(console_handler)
-
         return Logger.__logger
--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import os
 
 import torch
@@ -8,10 +10,12 @@ from d2l import torch as d2l
 from log import logger
 from models import LeNet5
 
-logger = logger.Logger().get_logger()
+LOG = logger.Logger().get_logger()
 base_dir = os.path.dirname((os.path.abspath(__file__)))
 TRAIN = 'train'
 config = None
+date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+checkpoint_path = None
 
 
 def parse_yml_config(model_name):
@@ -20,8 +24,8 @@ def parse_yml_config(model_name):
         with open(config_path, 'r', encoding='utf-8') as f:
             return yaml.load(f, Loader=yaml.FullLoader)
     except Exception as e:
-        logger.error(f'open config file error, file={config_path}')
-        logger.error(e)
+        LOG.error(f'open config file error, file={config_path}')
+        LOG.error(e)
 
     return None
 
@@ -45,27 +49,15 @@ def evaluate_accuracy_gpu(model, data_iter, device=None):
     return metric[0] / metric[1]
 
 
-def train_epoch(epoch, model, timer, train_iter, optimizer, device, loss_fun, test_iter):
-    # 训练损失之和，训练准确率之和，样本数
-    metric = d2l.Accumulator(3)
-    model.train()
-    timer.start()
-    for i, (X, y) in enumerate(train_iter):
-        optimizer.zero_grad()
-        X, y = X.to(device), y.to(device)
-        y_hat = model(X)
-        loss = loss_fun(y_hat, y)
-        loss.backward()
-        optimizer.step()
-        with torch.no_grad():
-            metric.add(loss * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
-
-    timer.stop()
-    train_loss = metric[0] / metric[2]
-    train_acc = metric[1] / metric[2]
-    test_acc = evaluate_accuracy_gpu(model, test_iter)
-    logger.info(
-        f'epoch={epoch + 1} time={timer.times[-1]:.2f}s train_loss={train_loss:.2f} train_acc={train_acc:.3f} test_acc={test_acc:.3f}')
+def save_checkpoint(epoch, net: nn.Module, optimizer, loss):
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    torch.save({
+        'epoch': epoch,
+        'net': net.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'loss_func': loss,
+    }, checkpoint_path + '/' + str(epoch) + '.pt')
 
 
 def train(model):
@@ -80,11 +72,12 @@ def train(model):
     device = config[TRAIN]['device']
     train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
     model.apply(init_weights)
-    logger.info(f'training on {device}')
     model.to(device)
     optimizer = model.optim()
     loss_fun = model.loss_func()
     timer, num_batches = d2l.Timer(), len(train_iter)
+    best_test_acc = 0.0
+    best = ''
     for epoch in range(epochs):
         # 训练损失之和，训练准确率之和，样本数
         metric = d2l.Accumulator(3)
@@ -104,15 +97,27 @@ def train(model):
         train_loss = metric[0] / metric[2]
         train_acc = metric[1] / metric[2]
         test_acc = evaluate_accuracy_gpu(model, test_iter)
-        logger.info(
-            f'epoch={epoch + 1} time={timer.times[-1]:.2f}s train_loss={train_loss:.2f} train_acc={train_acc:.3f} test_acc={test_acc:.3f}')
-
-    logger.info(f'total time: {timer.sum():.1f}s')
-    logger.info(f'{metric[2] * epochs / timer.sum():.1f} examples/sec on {str(device)}')
+        mm, ss = divmod(int(timer.times[-1]), 60)
+        hh, mm = divmod(mm, 60)
+        LOG.info(
+            f'epoch={epoch + 1}/{epochs} time={hh}h {mm}m {ss}s '
+            f'train_loss={train_loss:.2f} train_acc={train_acc * 100:.3f}% test_acc={test_acc * 100:.3f}%')
+        if test_acc > best_test_acc:
+            best_test_acc = test_acc
+            best = f'epoch={epoch + 1} train_loss={train_loss:.2f} ' \
+                   f'train_acc={train_acc * 100:.3f}% test_acc={test_acc * 100:.3f}%'
+        save_checkpoint(epoch + 1, model, optimizer, loss_fun)
+    mm, ss = divmod(int(timer.sum()), 60)
+    hh, mm = divmod(mm, 60)
+    LOG.info(f'best: {best}')
+    LOG.info(f'total time: time={hh}h {mm}m {ss}s')
+    LOG.info(f'{metric[2] * epochs / timer.sum():.1f} examples/sec on {str(device)}')
 
 
 if __name__ == '__main__':
     config = parse_yml_config(LeNet5.LeNet5.__name__)
-    logger.info(config)
+    logger.Logger.set(LeNet5.LeNet5.__name__, date)
+    checkpoint_path = os.path.join(config['checkpoint_path'], LeNet5.LeNet5.__name__, date)
+    LOG.info('\n' + json.dumps(config, indent=4))
     leNet5 = LeNet5.LeNet5(config)
     train(leNet5)
