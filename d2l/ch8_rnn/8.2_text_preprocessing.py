19d41ce96aae447e3ad403d9f2e6ef5b632e86ee
yinxiaolog <15121849303@163.com>
2023-06-11 13:16:23 +0800

    learn ch8 rnn

---
+++ d2l/ch8_rnn/8.2_text_preprocessing.py
@@ -0,0 +1,97 @@
+import collections
+import re
+
+from d2l import torch as d2l
+
+
+def read_time_machine():
+    with open('../data/timemachine.txt', 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+
+lines = read_time_machine()
+print(f'# 文本总行数：{len(lines)}')
+print(lines[0])
+print(lines[10])
+
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print('错误：未知词元类型：' + token)
+
+
+tokens = tokenize(lines)
+for i in range(11):
+    print(tokens[i])
+
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+class Vocab:
+    def __init__(self, tokens=None, min_freq=0, reserved_token=None):
+        if tokens is None:
+            tokens = []
+        if reserved_token is None:
+            reserved_token = []
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        self.idx_to_token = ['<unk>'] + reserved_token
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    @property
+    def unk(self):
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+
+vocab = Vocab(tokens)
+print(list(vocab.token_to_idx.items())[:10])
+
+for i in [0, 10]:
+    print(i)
+    print('text:', tokens[i])
+    print('索引:', vocab[tokens[i]])
+
+
+def load_corpus_time_machine(max_tokens=-1):  # @save
+    """返回时光机器数据集的词元索引列表和词表"""
+    lines = read_time_machine()
+    tokens = d2l.tokenize(lines, 'char')
+    vocab = d2l.Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落，
+    # 所以将所有文本行展平到一个列表中
+
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+
+corpus, vocab = load_corpus_time_machine()
+print(len(corpus), len(vocab))
---
+++ d2l/ch8_rnn/8.3_language_model_&_dataset.py
@@ -0,0 +1,25 @@
+import random
+
+import torch
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        return corpus[pos: pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i: i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+my_seq = list(range(35))
+for X, Y in seq_data_iter_random(my_seq, batch_size=2, num_steps=5):
+    print('X: ', X, '\nY: ', Y)
