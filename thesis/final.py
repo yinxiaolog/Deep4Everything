bf3fc14ee30c1bbfc1b839f9d2cf6c20bc2969ec
yinxiaolog <15121849303@163.com>
2024-04-27 23:17:36 +0800

    判断选择题

--- thesis/final.py
+++ thesis/final.py
@@ -1,59 +1,70 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import json
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 device = 'cuda'
 
 model_path = 'Qwen/Qwen1.5-14B-Chat'
-qwen14B = AutoModelForCausalLM.from_pretrained(
+model = AutoModelForCausalLM.from_pretrained(
     model_path, torch_dtype='auto', device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-prompt = '回答以下选择题，给出选项'
-messages = [
-    {
-        "role": "system",
-        "content": "以下是关于Dota2游戏知识的单项选择题，请选出其中的正确答案。"
-    },
-    {
-        "role": "user",
-        "content": "以下背景故事属于哪个物品？位居中央的宝石依然能反射出其制造者的景象。\nA. 魔杖\nB. 莲花\nC. A杖\nD. 天鹰之戒\n答案："
-    },
-    {
-        "role": "assistant",
-        "content": "A"
-    },
-    {
-        "role": "user",
-        "content": "亚巴顿的初始移动速度是多少？\nA. 305\nB. 310\nC. 295\nD. 290\n答案："
-    },
-    {
-        "role": "assistant",
-        "content": "B"
-    },
-    {
-        "role": "user",
-        "content": "梦境缠绕的冷却时间是多久？\nA. 100\nB. 6\nC. 20/15/10/5\nD. 70/65/60\n答案："
-    },
-    {
-        "role": "assistant",
-        "content": "D"
-    },
-    {
-        "role": "user",
-        "content": "以下背景故事属于哪个物品？这个有魔力的球体曾经保护了一位历史上最有名的英雄\nA. 狂战斧\nB. 死灵书\nC. 恢复指环\nD. 林肯法球\n答案："
-    },
-    {
-        "role": "assistant",
-        "content": ""
-    }
-]
 
-text = tokenizer.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True)
+def format_example(exam, include_answer=True, cot=False, add_prompt=''):
+    chat = []
+    CHOICES = 'ABCDEFGHIJK'
+    example = add_prompt + exam['question']
+    for i, choice in enumerate(exam['choices']):
+        example += f'\n{CHOICES[i]}. {choice}'
+    
+    chat_answer = {"role": "assistant"}
+    example += '\n答案：'
+    chat.append({"role": "user", "content": example})
+    if include_answer:
+        if cot:
+            ans = "让我们一步一步思考，\n" + \
+                exam["explanation"] + f"\n所以答案是{exam['answer']}。"
+        else:
+            ans = exam["answer"]
+        chat_answer["content"] = f'\n{ans}'
+    chat.append(chat_answer)
+    return chat
 
-model_inputs = tokenizer([text], return_tensors='pt').to(device)
-generated_ids = qwen14B.generate(model_inputs.input_ids, max_new_tokens=512)
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+
+def generate_few_shot_prompt(few_shot=[], cot=False):
+    chat = [
+        {"role": "system", "content": "以下是关于游戏Dota2知识考试的单项选择题，请选出其中正确的答案。\n\n"}
+    ]
+    for exam in few_shot:
+        chat.extend(format_example(exam, cot=cot))
+    return chat
+
+def judge(outputs: str, answer: str):
+    outputs = outputs.strip()
+    return outputs == answer
+
+
+def dota2_eval(file, model, tokenizer):
+    with open(file, 'r') as f:
+        exams = json.load(f)
+    few_shot = exams[0: 4]
+    chat = generate_few_shot_prompt(few_shot, cot=False)
+    for c in chat:
+        print(c['role'])
+        print(c['content'], end='')
+    return
+    right = 0
+    for i in range(4, len(exams)):
+        chat_one = format_example(exams[i], include_answer=False, cot=False)
+        chat_tmp = chat
+        chat_tmp.append(chat_one)
+        formatted_chat = tokenizer.apply_chat_template(chat_tmp, tokenize=False, add_generation_prompt=True)
+        inputs = tokenizer(formatted_chat, return_tensors="pt",add_special_tokens=False)
+        inputs = {key: tensor.to(model.device) for key, tensor in inputs.items()}
+        outputs = model.generate(**inputs, max_new_tokens=512)
+        decoded_output = tokenizer.decode(outputs[0][inputs['input_ids'].size(1):], skip_special_tokens=True)
+        if judge(decoded_output, exams[i]):
+            right += 1
+    print("acc=", right / (len(exams) - 3))
+
+dota2_eval('/data/yinxiaoln/datasets/dota2eval/data2-eval.json', model, tokenizer)
